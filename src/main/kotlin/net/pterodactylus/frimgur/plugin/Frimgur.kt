8c1c5d3a5a2294c994ef33c98dc966f901cc8784
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2023-01-02 15:51:28 +0100

    🚧 Implement FredPluginL10n interface

--- src/main/kotlin/net/pterodactylus/frimgur/plugin/Frimgur.kt
+++ src/main/kotlin/net/pterodactylus/frimgur/plugin/Frimgur.kt
@@ -1,15 +1,29 @@
 package net.pterodactylus.frimgur.plugin
 
+import freenet.l10n.BaseL10n.LANGUAGE
 import freenet.pluginmanager.FredPlugin
+import freenet.pluginmanager.FredPluginL10n
 import freenet.pluginmanager.PluginRespirator
+import java.util.Locale
+import java.util.ResourceBundle
 
 /**
  * Frimgur main plugin class.
  */
-class Frimgur : FredPlugin {
+class Frimgur : FredPlugin, FredPluginL10n {
 
 	override fun runPlugin(pluginRespirator: PluginRespirator) = Unit
 
 	override fun terminate() = Unit
+
+	override fun setLanguage(newLanguage: LANGUAGE) {
+		this.language = newLanguage
+	}
+
+	override fun getString(key: String): String {
+		return ResourceBundle.getBundle("i18n/frimgur", Locale.forLanguageTag(language.shortCode)).getString(key)
+	}
+
+	private var language = LANGUAGE.ENGLISH
 
 }
--- src/test/kotlin/net/pterodactylus/frimgur/plugin/FrimgurTest.kt
+++ src/test/kotlin/net/pterodactylus/frimgur/plugin/FrimgurTest.kt
@@ -1,8 +1,11 @@
 package net.pterodactylus.frimgur.plugin
 
+import freenet.l10n.BaseL10n
 import freenet.pluginmanager.FredPlugin
+import freenet.pluginmanager.FredPluginL10n
 import freenet.pluginmanager.PluginRespirator
 import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
 import org.hamcrest.Matchers.instanceOf
 import org.mockito.kotlin.mock
 import kotlin.test.Test
@@ -20,19 +23,36 @@ class FrimgurTest {
 	@Test
 	fun `Frimgur instance can be run`() {
 		val pluginRespirator = mock<PluginRespirator>()
-		val frimgur = Frimgur()
 		frimgur.runPlugin(pluginRespirator)
 	}
 
 	@Test
 	fun `Frimgur instance can be terminated`() {
-		val frimgur = Frimgur()
 		frimgur.terminate()
 	}
 
 	@Test
 	fun `Frimgur implements FredPlugin`() {
-		assertThat(Frimgur(), instanceOf(FredPlugin::class.java))
+		assertThat(frimgur, instanceOf(FredPlugin::class.java))
 	}
+
+	@Test
+	fun `Frimgur can deliver translated keys for English language`() {
+		frimgur.setLanguage(BaseL10n.LANGUAGE.ENGLISH);
+		assertThat(frimgur.getString("Test.Message"), equalTo("Test Message"))
+	}
+
+	@Test
+	fun `Frimgur can deliver translated keys for German language`() {
+		frimgur.setLanguage(BaseL10n.LANGUAGE.GERMAN);
+		assertThat(frimgur.getString("Test.Message"), equalTo("Testnachricht"))
+	}
+
+	@Test
+	fun `Frimgur implements FredPluginL10n`() {
+		assertThat(frimgur, instanceOf(FredPluginL10n::class.java))
+	}
+
+	private val frimgur = Frimgur()
 
 }

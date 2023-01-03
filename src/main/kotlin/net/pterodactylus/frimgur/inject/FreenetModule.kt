cc9aec9fa782f573f0973f7108f9559f3919167a
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2023-01-03 10:42:34 +0100

    🚧 Let the FreenetModule provide the ToadletContainer

--- src/main/kotlin/net/pterodactylus/frimgur/inject/FreenetModule.kt
+++ src/main/kotlin/net/pterodactylus/frimgur/inject/FreenetModule.kt
@@ -3,6 +3,7 @@ package net.pterodactylus.frimgur.inject
 import com.google.inject.AbstractModule
 import com.google.inject.Provides
 import freenet.clients.http.PageMaker
+import freenet.clients.http.ToadletContainer
 import freenet.pluginmanager.FredPluginL10n
 import freenet.pluginmanager.PluginRespirator
 import net.pterodactylus.frimgur.plugin.Frimgur
@@ -17,5 +18,8 @@ class FreenetModule(frimgur: Frimgur, pluginRespirator: PluginRespirator) : Abst
 
 	@get:Provides
 	val pluginL10n: FredPluginL10n = frimgur
+
+	@get:Provides
+	val toadletContainer: ToadletContainer = pluginRespirator.toadletContainer
 
 }
--- src/test/kotlin/net/pterodactylus/frimgur/inject/FreenetModuleTest.kt
+++ src/test/kotlin/net/pterodactylus/frimgur/inject/FreenetModuleTest.kt
@@ -2,6 +2,7 @@ package net.pterodactylus.frimgur.inject
 
 import com.google.inject.Guice.createInjector
 import freenet.clients.http.PageMaker
+import freenet.clients.http.ToadletContainer
 import freenet.pluginmanager.FredPluginL10n
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.notNullValue
@@ -16,16 +17,20 @@ class FreenetModuleTest {
 
 	@Test
 	fun `freenet module can provide page maker`() {
-		val injector = createInjector(module)
 		assertThat(injector.getInstance(PageMaker::class.java), notNullValue())
 	}
 
 	@Test
 	fun `freenet module can provide plugin L10n`() {
-		val injector = createInjector(module)
 		assertThat(injector.getInstance(FredPluginL10n::class.java), notNullValue())
 	}
 
+	@Test
+	fun `freenet module can provide toadlet container`() {
+		assertThat(injector.getInstance(ToadletContainer::class.java), notNullValue())
+	}
+
 	private val module = FreenetModule(mock(), mock(defaultAnswer = RETURNS_DEEP_STUBS))
+	private val injector = createInjector(module)
 
 }

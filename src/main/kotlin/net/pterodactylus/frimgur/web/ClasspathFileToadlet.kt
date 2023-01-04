7b6854ffd7dea887fb093d46922ca8ec9fe0d48a
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2023-01-04 15:53:55 +0100

    🚧 Add classpath file toadlet implementation

---
+++ src/main/kotlin/net/pterodactylus/frimgur/web/ClasspathFileToadlet.kt
@@ -0,0 +1,27 @@
+package net.pterodactylus.frimgur.web
+
+import freenet.client.HighLevelSimpleClient
+
+/**
+ * [File toadlet][FileToadlet] implementation that can deliver files from a location on the classpath.
+ */
+class ClasspathFileToadlet(stripPrefix: String, private val addPrefix: String, highLevelSimpleClient: HighLevelSimpleClient) : FileToadlet(stripPrefix, highLevelSimpleClient) {
+
+	override fun locateFileData(file: String): FileData? =
+		javaClass.getResourceAsStream(addPrefix + file)
+			?.let { FileData(-1, null, it) }
+			?.detectMimeType(file)
+
+	private fun FileData.detectMimeType(filename: String) =
+		mimeTypeFilters
+			.firstOrNull { (filter, mimeType) -> filter(filename) }
+			?.let { this.copy(mimeType = it.second) }
+			?: this
+
+	fun addMimeType(mimeType: String, filenameFilter: (filename: String) -> Boolean) {
+		mimeTypeFilters += (filenameFilter to mimeType)
+	}
+
+	private val mimeTypeFilters = mutableListOf<Pair<(String) -> Boolean, String>>()
+
+}
---
+++ src/test/kotlin/net/pterodactylus/frimgur/web/ClasspathFileToadletTest.kt
@@ -0,0 +1,44 @@
+package net.pterodactylus.frimgur.web
+
+import freenet.client.HighLevelSimpleClient
+import freenet.clients.http.ToadletContext
+import org.mockito.AdditionalMatchers.or
+import org.mockito.kotlin.any
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.isNull
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import java.net.URI
+import kotlin.test.Test
+
+/**
+ * Unit test for [ClasspathFileToadlet].
+ */
+class ClasspathFileToadletTest {
+
+	@Test
+	fun `toadlet cannot serve files that are not there`() {
+		val toadlet = ClasspathFileToadlet("/prefix/", "/server/", highLevelSimpleClient)
+		toadlet.handleMethodGET(URI("/prefix/does-not-exist.dat"), mock(), toadletContext)
+		verify(toadletContext).sendReplyHeaders(eq(404), any(), any(), or(any(), isNull()), any())
+	}
+
+	@Test
+	fun `toadlet can serve file from classpath`() {
+		val toadlet = ClasspathFileToadlet("/prefix/", "/server/", highLevelSimpleClient)
+		toadlet.handleMethodGET(URI("/prefix/test.dat"), mock(), toadletContext)
+		verify(toadletContext).sendReplyHeaders(eq(200), any(), any(), or(any(), isNull()), any())
+	}
+
+	@Test
+	fun `toadlet can serve mime types for specified file extensions`() {
+		val toadlet = ClasspathFileToadlet("/prefix/", "/server/", highLevelSimpleClient)
+		toadlet.addMimeType("application/x-test-data") { filename: String -> filename.endsWith(".dat", ignoreCase = true) }
+		toadlet.handleMethodGET(URI("/prefix/test.dat"), mock(), toadletContext)
+		verify(toadletContext).sendReplyHeaders(eq(200), any(), any(), eq("application/x-test-data"), any())
+	}
+
+	private val highLevelSimpleClient = mock<HighLevelSimpleClient>()
+	private val toadletContext = mock<ToadletContext>()
+
+}

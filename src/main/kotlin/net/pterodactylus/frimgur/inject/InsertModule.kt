5424d7337e239a9d35c57231cf02b4251fda3a1d
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2023-01-10 22:25:30 +0100

    🚧 Provide insert service as singleton

--- src/main/kotlin/net/pterodactylus/frimgur/inject/InsertModule.kt
+++ src/main/kotlin/net/pterodactylus/frimgur/inject/InsertModule.kt
@@ -5,6 +5,7 @@ import com.google.inject.Provides
 import freenet.client.HighLevelSimpleClient
 import net.pterodactylus.frimgur.insert.DefaultInsertService
 import net.pterodactylus.frimgur.insert.InsertService
+import javax.inject.Singleton
 
 /**
  * Guice [Module] that configures insert-related functionality.
@@ -12,6 +13,7 @@ import net.pterodactylus.frimgur.insert.InsertService
 class InsertModule : AbstractModule() {
 
 	@Provides
+	@Singleton
 	fun getInsertService(highLevelSimpleClient: HighLevelSimpleClient): InsertService =
 		DefaultInsertService(highLevelSimpleClient)
 
--- src/test/kotlin/net/pterodactylus/frimgur/inject/InsertModuleTest.kt
+++ src/test/kotlin/net/pterodactylus/frimgur/inject/InsertModuleTest.kt
@@ -2,9 +2,12 @@ package net.pterodactylus.frimgur.inject
 
 import com.google.inject.Guice.createInjector
 import freenet.client.HighLevelSimpleClient
+import net.pterodactylus.frimgur.insert.InsertService
 import net.pterodactylus.frimgur.test.bind
+import net.pterodactylus.frimgur.util.getInstance
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.notNullValue
+import org.hamcrest.Matchers.sameInstance
 import kotlin.test.Test
 
 /**
@@ -19,7 +22,14 @@ class InsertModuleTest {
 
 	@Test
 	fun `guice module can create insert service`() {
-		assertThat(injector.getInstance(InsertModule::class.java), notNullValue())
+		assertThat(injector.getInstance<InsertModule>(), notNullValue())
+	}
+
+	@Test
+	fun `insert service is provided as singleton`() {
+		val firstInstance: InsertService = injector.getInstance()
+		val secondInstance: InsertService = injector.getInstance()
+		assertThat(secondInstance, sameInstance(firstInstance))
 	}
 
 	private val injector = createInjector(

5bd3eb05e90d597bbb3c22b14e1d31bbb8ccb867
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2023-01-11 11:14:47 +0100

    🚚 Move HTML files to static/ prefix

--- src/main/kotlin/net/pterodactylus/frimgur/web/MainPageProcessor.kt
+++ src/main/kotlin/net/pterodactylus/frimgur/web/MainPageProcessor.kt
@@ -6,7 +6,7 @@ import net.pterodactylus.frimgur.web.annotations.ToadletName
  * Frimgur main page.
  */
 @ToadletName("index.html")
-class MainPageProcessor(formPassword: String) : PebblePageProcessor("html/main.html", formPassword) {
+class MainPageProcessor(formPassword: String) : PebblePageProcessor("static/html/main.html", formPassword) {
 
 	override fun getTitle(pageRequest: PageRequest) = "Frimgur Main Page"
 	override fun getScriptLinks(pageRequest: PageRequest) = listOf("static/js/frimgur.js")

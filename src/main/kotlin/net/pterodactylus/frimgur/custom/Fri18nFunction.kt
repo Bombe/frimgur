af7e5c11342d3e70285004283be1c32138fde3f5
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2023-01-28 23:56:16 +0100

    🚧 Add custom i18n function
    The original i18n cannot use a custom class loader which is unusable for
    Frimgur, as Pebble and Frimgur are loaded with different class loaders.

---
+++ src/main/kotlin/net/pterodactylus/frimgur/custom/Fri18nFunction.kt
@@ -0,0 +1,43 @@
+package net.pterodactylus.frimgur.custom
+
+import com.mitchellbosecke.pebble.extension.AbstractExtension
+import com.mitchellbosecke.pebble.extension.Extension
+import com.mitchellbosecke.pebble.extension.Function
+import com.mitchellbosecke.pebble.template.EvaluationContext
+import com.mitchellbosecke.pebble.template.PebbleTemplate
+import java.text.MessageFormat
+import java.util.ResourceBundle
+
+/**
+ * Custom Pebble [Function] to make sure the correct class loader is used to
+ * load [ResourceBundle]s.
+ */
+class Fri18nFunction(private var classLoader: ClassLoader) : Function {
+
+	override fun getArgumentNames() = cachedArgumentNames
+
+	override fun execute(args: Map<String, Any>, self: PebbleTemplate, context: EvaluationContext, lineNumber: Int): Any? {
+		val bundle = args["bundle"] as? String ?: return null
+		val key = args["key"] as? String ?: return null
+		val params = args["params"]
+
+		val resourceBundle = ResourceBundle.getBundle(bundle, context.locale, classLoader)
+		val message = resourceBundle.getObject(key).toString()
+		if (params is List<Any?>) {
+			return MessageFormat.format(message, *params.toTypedArray())
+		} else if (params != null) {
+			return MessageFormat.format(message, params)
+		}
+		return message
+	}
+
+}
+
+/**
+ * Returns a Pebble [Extension] that will install [Fri18nFunction] under the name `fri18n`.
+ */
+fun fri18nExtension(classLoader: ClassLoader = Fri18nFunction::class.java.classLoader) = object : AbstractExtension() {
+	override fun getFunctions(): Map<String, Function> = mapOf("fri18n" to Fri18nFunction(classLoader))
+}
+
+private val cachedArgumentNames = listOf("bundle", "key", "params")
---
+++ src/test/kotlin/net/pterodactylus/frimgur/custom/Fri18nFunctionTest.kt
@@ -0,0 +1,60 @@
+package net.pterodactylus.frimgur.custom
+
+import com.mitchellbosecke.pebble.PebbleEngine
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.contains
+import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.sameInstance
+import java.io.StringWriter
+import java.util.Locale.ENGLISH
+import kotlin.test.Test
+
+/**
+ * Unit test for [Fri18nFunction].
+ */
+class Fri18nFunctionTest {
+
+	@Test
+	fun `fri18n function exposes argument names`() {
+		assertThat(fri18nFunction.argumentNames, contains("bundle", "key", "params"));
+	}
+
+	@Test
+	fun `argument names list is cached`() {
+		val firstArgumentNames = fri18nFunction.argumentNames
+		val secondArgumentNames = fri18nFunction.argumentNames
+		assertThat(firstArgumentNames, sameInstance(secondArgumentNames))
+	}
+
+	@Test
+	fun `function returns value from bundle`() {
+		val template = pebbleEngine.getLiteralTemplate("{{ fri18n('i18n', 'Test.Message') }}")
+		val output = StringWriter().apply { template.evaluate(this, ENGLISH) }.toString()
+		assertThat(output, equalTo("Test Message"))
+	}
+
+	@Test
+	fun `function can format message with one parameter`() {
+		val template = pebbleEngine.getLiteralTemplate("{{ fri18n('i18n', 'Test.WithOneParam', 'a') }}")
+		val output = StringWriter().apply { template.evaluate(this, ENGLISH) }.toString()
+		assertThat(output, equalTo("Parameter a"))
+	}
+
+	@Test
+	fun `function can format message with two parameters`() {
+		val template = pebbleEngine.getLiteralTemplate("{{ fri18n('i18n', 'Test.WithTwoParams', ['a', 'b']) }}")
+		val output = StringWriter().apply { template.evaluate(this, ENGLISH) }.toString()
+		assertThat(output, equalTo("Parameters a and b"))
+	}
+
+	private val fri18nFunction = Fri18nFunction(testClassLoader)
+
+}
+
+private val testClassLoader = object : ClassLoader(Fri18nFunctionTest::class.java.classLoader) {
+	override fun getResource(name: String?) = super.getResource("net/pterodactylus/frimgur/custom/test_$name")
+}
+
+private val pebbleEngine = PebbleEngine.Builder()
+	.extension(fri18nExtension(testClassLoader))
+	.build()
